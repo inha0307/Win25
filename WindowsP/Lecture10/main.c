c1f6a85f98e91dc25e6088edcc9b51ac8e40ed65
inha0307 <emforhs033@naver.com>
2025-05-13 15:57:38 +0900

    Circle_Draw

--- WindowsP/Lecture10/main.c
+++ WindowsP/Lecture10/main.c
@@ -2,36 +2,45 @@
 #include <GLFW/glfw3.h>
 #include <math.h>
 
-void drawCircle(float cx, float cy, float r, int num_segments) {
+void drawCircle(float cx, float cy, float r, int num_segments) 
+{
     int i;
     glBegin(GL_TRIANGLE_FAN);
-    glVertex2f(cx, cy); // 원의 중심
-    for (i = 0; i <= num_segments; i++) {
-        float angle = 2.0f * 3.14159265358979323846f * (float)i / (float)num_segments; // 각도 계산 (라디안)
-        float x = cx + r * cos(angle); // x 좌표 계산
-        float y = cy + r * sin(angle); // y 좌표 계산
+    glColor3f(1.0f, 1.0f, 0.0f);
+    glVertex2f(cx, cy);
+    for (i = 0; i <= num_segments; i++) 
+    {
+        float angle = 2.0f * 3.14159265358979323846f * (float)i / (float)num_segments;
+        float x = cx + r * cos(angle);
+        float y = cy + r * sin(angle);
+        glColor3f(1.0f, 1.0f, 1.0f);
         glVertex2f(x, y);
     }
     glEnd();
 }
 
-int main() {
-    if (!glfwInit()) return -1;
+int main() 
+{
+    if (!glfwInit())
+        return -1;
 
     GLFWwindow* window = glfwCreateWindow(500, 500, "GLFW: CIRCLE", NULL, NULL);
-    if (!window) {
+
+    if (!window) 
+    {
         glfwTerminate();
         return -1;
     }
 
     glfwMakeContextCurrent(window);
-    
-    while (!glfwWindowShouldClose(window)) {
-        drawCircle(0.0f, 0.0f, 0.5f, 50);
+
+    while (!glfwWindowShouldClose(window)) 
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+        drawCircle(0.0f, 0.0f, 0.8f, 100);
         glfwSwapBuffers(window);
         glfwPollEvents();
     }
-
     glfwDestroyWindow(window);
     glfwTerminate();
     return 0;
